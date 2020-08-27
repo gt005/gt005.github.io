01975e03ceb0ef7f0c34db0cffe78a9e82ab7df6
gt005 <karimhamid@yandex.ru>
2020-08-28 01:07:16 +0300

    Сделал выезжающее окно header

--- watch group/js/main.js
+++ watch group/js/main.js
@@ -24,6 +24,15 @@ $(document).ready(function () {
     })
 });
 
+document.querySelector('.open-mobile-menu-btn').onclick = function () {
+    let subHeader = document.querySelector('.sub-header'),
+        subHeaderContainer = document.querySelector('.sub-header-container');
+
+    subHeaderContainer.style.left = '0';
+    subHeaderContainer.style.opacity = '1';
+    subHeader.style.transform = "translateX(0%)";
+}
+
 document.querySelector('.close-mobile-header-btn').onclick = function () {
     let subHeader = document.querySelector('.sub-header'),
         subHeaderContainer = document.querySelector('.sub-header-container');
@@ -35,12 +44,24 @@ document.querySelector('.close-mobile-header-btn').onclick = function () {
     }, 200)
 }
 
-document.querySelector('.open-mobile-menu-btn').onclick = function () {
-    let subHeader = document.querySelector('.sub-header'),
-        subHeaderContainer = document.querySelector('.sub-header-container');
 
-    subHeaderContainer.style.left = '0';
-    subHeaderContainer.style.opacity = '1';
-    subHeader.style.transform = "translateX(0%)";
+
+document.querySelector('.header-shopping-cart').onclick = function () {
+    let shoppingCartDropdown = document.querySelector('.shopping-cart-dropdown'),
+        shoppingCartDropdownContainer = document.querySelector('.shopping-cart-dropdown-container');
+
+    shoppingCartDropdownContainer.style.left = '0';
+    shoppingCartDropdownContainer.style.opacity = '1';
+    shoppingCartDropdown.style.transform = "translateX(0%)";
 }
 
+document.querySelector('.shopping-cart-dropdown-container').onclick = function () {
+    let shoppingCartDropdown = document.querySelector('.shopping-cart-dropdown');
+
+    shoppingCartDropdown.style.transform = "";
+    this.style.opacity = '0';
+    // setTimeout(function () {
+    //     this.style.left = '-150%';
+    // }, 200)
+
+}
