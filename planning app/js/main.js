3520a6339d5e3aa6c677129e843236efb7cfacef
Karim <karimhamid@yandex.ru>
2021-01-02 02:24:03 +0300

    Сделаны section до resources в 'planning app'

--- planning app/js/main.js
+++ planning app/js/main.js
@@ -1,4 +1,3 @@
-
 $(window).ready(function () {
     $('#welcome-section__carousel').owlCarousel({
         loop:true,
@@ -10,5 +9,18 @@ $(window).ready(function () {
         autoplay:true,
         autoplayHoverPause:true,
         smartSpeed:700
+    })
+
+    $('#resources__carousel').owlCarousel({
+        loop:true,
+        nav:true,
+        dots:true,
+        touchDrag:true,
+        items:1,
+        autoplayTimeout:75000,
+        autoplay:true,
+        autoplayHoverPause:true,
+        smartSpeed:700,
+        navText: ["<svg width=\"27\" height=\"19\" viewBox=\"0 0 27 19\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M25 9.5L2 9.5M2 9.5L9.81132 17M2 9.5L9.81132 2\" stroke=\"#A8B4E5\" stroke-width=\"3\" stroke-linecap=\"round\" stroke-linejoin=\"round\"/></svg>", "<svg width=\"27\" height=\"19\" viewBox=\"0 0 27 19\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M2 9.5H25M25 9.5L17.1887 2M25 9.5L17.1887 17\" stroke=\"#A8B4E5\" stroke-width=\"3\" stroke-linecap=\"round\" stroke-linejoin=\"round\"/></svg>"]
     })
 });
