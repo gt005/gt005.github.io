34be7b7b3ac67165dc3bdb18bbc67159c88c6561
Karim <karimhamid@yandex.ru>
2021-02-12 19:13:49 +0300

    IT класс: сделан главный экран

---
+++ landing_for_it_class/js/main.js
@@ -0,0 +1,30 @@
+const mainContainer = document.querySelector(".main-section");
+
+for (let i = 0; i < 35; i++) {
+    const blocks = document.createElement('div');
+    blocks.classList.add('main-section__bg-block')
+    mainContainer.appendChild(blocks);
+}
+
+let elements = document.querySelectorAll('.main-section__bg-block');
+
+function animateMainBgBlocks() {
+    anime({
+        targets: elements,
+        translateX: function () {
+            return anime.random(-700, 700)
+        },
+        translateY: function () {
+            return anime.random(-400, 400)
+        },
+        scale: function () {
+            return anime.random(1, 5)
+        },
+        easing: "easeInQuad",
+        duration: 3000,
+        delay: 500,
+        complete: animateMainBgBlocks,
+    });
+}
+
+animateMainBgBlocks();
