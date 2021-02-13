734f2645233d641a9e442aee1ff9dc6efd26ee5b
Karim <karimhamid@yandex.ru>
2021-02-13 17:23:15 +0300

    IT класс: Сделана страница

--- landing_for_it_class/js/main.js
+++ landing_for_it_class/js/main.js
@@ -27,4 +27,16 @@ function animateMainBgBlocks() {
     });
 }
 
-animateMainBgBlocks();
+// animateMainBgBlocks();
+
+
+let timeTableRow = document.querySelectorAll(".timetable__dates__events-list__element");
+
+
+timeTableRow.forEach(function (button) {
+    button.querySelector('.timetable__dates__events-list__element__open-more').onclick = () => {
+        $(button.querySelector('.timetable__dates__events-list__element__more-text')).slideToggle();
+    }
+})
+
+// slideToggle
